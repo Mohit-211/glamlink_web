04acf88ffc079562af5d156a6c3891788c2e5386
bhavya8221 <bhavyasoni8221@gmail.com>
2026-03-02 18:02:35 +0530

    slove next js error

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,22 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  devIndicators: false,
+// next.config.ts
+import { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
   images: {
-    domains: ["node.glamlink.net"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "example.com",
+        port: "", // optional
+        pathname: "/**", // allow all paths
+      },
+      {
+        protocol: "https",
+        hostname: "cdn.example.com",
+        pathname: "/images/**", // restrict path if needed
+      },
+    ],
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
