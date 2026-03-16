471b1822f1f4eb0ed2d59974584e5182657b4322
bhavya8221 <bhavyasoni8221@gmail.com>
2026-03-16 17:19:52 +0530

    slove error in directory page

--- src/app/directory/page.tsx
+++ src/app/directory/page.tsx
@@ -1,5 +1,13 @@
+"use client"
 import DirectoryPage from "@/app/directory/DirectoryPage";
+import { Suspense } from "react";
 
 export default function DirectoryMainPage() {
-  return <DirectoryPage />;
+  return (
+
+    <Suspense fallback={null}>
+
+  <DirectoryPage />;
+  </Suspense>
+  )
 }
