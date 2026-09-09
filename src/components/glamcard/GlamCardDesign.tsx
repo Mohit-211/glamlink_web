1f686a8fb2b8dadf7ad26f16699a912a37aa301c
bhavya8221 <bhavyasoni8221@gmail.com>
2026-09-09 16:14:05 +0530

    fix padding

--- src/components/glamcard/GlamCardDesign.tsx
+++ src/components/glamcard/GlamCardDesign.tsx
@@ -15,7 +15,7 @@ const GlamCardDesign: React.FC<Props> = ({ slug }) => {
   }
 
   return (
-    <div className="max-w-4xl mx-auto p-4">
+    <div className="max-w-4xl mx-auto">
       <BusinessCardPage slug={slug} mode="view" />
     </div>
   );
